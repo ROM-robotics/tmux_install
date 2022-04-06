a0a816ae9da912d4b4490880c72992281f20b47b
ROM Robotics Myanmar <romrobotics@gmail.com>
2022-04-06 11:06:05 +0630

    first

---
+++ README.md
@@ -0,0 +1,10 @@
+# tmux
+
+for first use, uncomment these lines
+
+sudo apt install -y tmux tmuxinator
+mv ~/.tmux.conf ~/.tmux.conf.backup
+cp ./.tmux.conf ~/.tmux.conf
+echo "Backup and copy ~/.tmux.conf "
+
+<img src="tmux.png" width=800 height=500 />
---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+###     tmux CONFIGURATION
+#sudo apt install -y tmux tmuxinator
+#mv ~/.tmux.conf ~/.tmux.conf.backup
+#cp ./.tmux.conf ~/.tmux.conf
+#echo "Backup and copy ~/.tmux.conf "
+
+##      Absolute path to this script. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f $0)
+echo "$SCRIPT"
+
+###     Absolute path this script is in. /home/user/bin
+SCRIPTPATH=`dirname $SCRIPT`
+echo "$SCRIPTPATH"
+cd "$SCRIPTPATH"
+
+###     remove the old link
+rm .tmuxinator.yml
+
+###     link the session file to .tmuxinator.yml
+ln session.yml .tmuxinator.yml
+echo "Creating .tmuxinator.yaml link."
+
+###     start tmuxinator
+tmuxinator
