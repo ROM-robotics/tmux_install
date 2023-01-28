df9bcb7efe711ab41179b1064e90c92daf529e68
pyaesoanaung <romrobotics@gmail.com>
2023-01-28 07:14:10 +0630

    fix

--- start.sh
+++ start.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 
 ###     tmux CONFIGURATION
-sudo apt install -y tmux tmuxinator neofetch
-mv ~/.tmux.conf ~/.tmux.conf.backup
+sudo apt install -y tmux tmuxinator neofetch cowsay htop
+if [[ -e ~/.tmux.conf ]]
+then
+    rm ~/.tmux.conf
+fi
+
 cp ./.tmux.conf ~/.tmux.conf
-echo "Backup and copy ~/.tmux.conf "
 
-###     Installing additional
-sudo apt install cowsay
+if [[ -e ~/.tmuxinator.yml ]]
+then
+    rm ~/.tmuxinator.yml
+fi
 
-##      Absolute path to this script. /home/user/bin/foo.sh
-SCRIPT=$(readlink -f $0)
-echo "$SCRIPT"
-
-###     Absolute path this script is in. /home/user/bin
-SCRIPTPATH=`dirname $SCRIPT`
-echo "$SCRIPTPATH"
-cd "$SCRIPTPATH"
-
-###     remove the old link
-rm .tmuxinator.yml
-
-###     link the session file to .tmuxinator.yml
-ln session.yml .tmuxinator.yml
-echo "Creating .tmuxinator.yaml link."
-
-###     start tmuxinator
-tmuxinator
+cp ./.tmuxinator.yml ~/
+echo "Run tmuxinator from home directory."
